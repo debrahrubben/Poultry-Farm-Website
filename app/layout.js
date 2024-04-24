949e1d3b81bc5c6b7e4e51ee19ebf310526b11dd
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-04-24 18:37:50 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <head><meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       </head>
       <body className={inter.className}>{children}</body>
     </html>
