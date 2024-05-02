6c069f4f927a03d262e0366c4b17e0a04a579b10
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-05-02 16:41:37 +0000

    darkmode

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 
+import { ThemeProvider } from 'next-themes';
 import { Card } from "../components/component/card.jsx";
 import { Organic } from "../components/component/Organic.jsx";
 import { Gallery } from "../components/component/Gallery.jsx";
@@ -9,7 +10,8 @@ import { Services } from "../components/component/Services.jsx";
 
 export default function Home() {
   return (
-  <div style={{backgroundColor:'#DDDDDD'}}>
+    <ThemeProvider enableSystem={false} attribute="class">
+      <div style={{backgroundColor:'#DDDDDD'}}>
    <HeroSection />
    <Services />
    <Card />
@@ -19,6 +21,8 @@ export default function Home() {
    <AboutUs />
    <Footer />
    <div style={{ background: 'linear-gradient(to right, #8f2e37, #d48e52)', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)' }}></div>
-    </div>
+   </div>
+    </ThemeProvider>
+  
   );
 }
